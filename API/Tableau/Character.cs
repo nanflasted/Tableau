c0b3d4e9e9f6499c4e619631567a3dcea4859209
nanflasted <nanflasted@gmail.com>
2016-06-15 19:57:02 -0400

    first implementation of Character class

---
+++ API/Tableau/Character.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Tableau
+{
+	public abstract class Character : GameObject
+	{
+		public string name;
+		public string type;
+		public int ID;
+		//TODO public List</*unityAnimationType*/> animations;
+		//TODO public List</*unitySoundType*/> sounds;	
+		public Character owner;
+		public GameObject prefab;	
+		public Zone location;
+		
+
+		public virtual void spawn()
+		{
+			//acquire coordinates
+			//spawn prefab
+			//play spawn animation
+			//sound
+			
+		}
+		
+		public virtual void spawn(Vector3 pos, Quarternion rot)
+		{
+			Instantiate(prefab,pos,rot);
+			//play spawn animations
+			//play sound
+		}
+				
+
+		public virtual void spawn(Zone z)
+		{
+			//acquire coordinates from z
+			//spawn prefab
+			//play spawn animation
+			//play sound
+		}
+
+		public virtual void destruct()
+		{
+		}
+
+		public virtual void moveToZone(Zone z)
+		{
+			//visual effect triggered by a Motion or Motions
+		}
+
+		public virtual void playAnimation(int n)
+		{
+		}
+
+		public virtual void playSound(int n)
+		{
+		}
+
+		public virtual void triggerMotion(Motion m)
+		{
+		}
+
+		
+	}
+}
