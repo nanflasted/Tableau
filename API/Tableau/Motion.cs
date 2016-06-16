991f963e572e689301cd81f501752771f1e7cf57
James Hoak <jah272@case.edu>
2016-06-16 00:59:57 -0400

    extra comments for good measure
    <|8O)

--- API/Tableau/Motion.cs
+++ API/Tableau/Motion.cs
@@ -29,32 +29,44 @@ namespace Tableau {
 
 		// Factories
 		public static Motion Make(Zone initiator, Zone target) {
+			// Check input
 			if (initiator == null || target == null)
 				throw new MotionInitException("Attempted to make motion with null initiator/target");
+			
+			// If input is good...
 			return new Motion(initiator, target);
 		}
 
 		public static Motion Make(Zone[] initiators, Zone[] targets) {
+			// Check input
 			if (initiators == null || targets == null)
 				throw new MotionInitException("Attempted to make motion with null initiator/target");
 			if (getNumOccupants(initiators) == 0 || getNumOccupants(targets) == 0)
 				throw new MotionInitException("Attempted to make motion with empty set of initiators/targets");
+			
+			// If input is good...
 			return new Motion(initiators, targets);
 		}
 
 		public static Motion Make(Zone initiator, Zone[] targets) {
+			// Check input
 			if (initiator == null || targets == null)
 				throw new MotionInitException("Attempted to make motion with null initiator/target");
 			if (getNumOccupants(targets) == 0)
 				throw new MotionInitException("Attempted to make motion with empty set of targets");
+			
+			// If input is good...
 			return new Motion(initiator, targets);
 		}
 
 		public static Motion Make(Zone[] initiators, Zone target) {
+			// Check input
 			if (initiators == null || target == null)
 				throw new MotionInitException("Attempted to make motion with null initiator/target");
 			if (getNumOccupants(initiators) == 0)
 				throw new MotionInitException("Attempted to make motion with empty set of initiators");
+			
+			// If input is good...
 			return new Motion(initiators, target);
 		}
 
@@ -63,30 +75,42 @@ namespace Tableau {
 		public Zone[] getTargets() { return targets; }
 
 		protected internal void setInitiators(Zone initiator) {
+			// Check input
 			if (initiator == null)
 				throw new MotionChangeException("Tried to set initiator to null zone");
+			
+			// If input is good...
 			initiators = ToSingletonArray(initiator);
 		}
 
 		protected internal void setInitiators(Zone[] initiators) {
+			// Check input
 			if (initiators == null)
 				throw new MotionChangeException("Tried to set initiator to null zones");
 			if (getNumOccupants(initiators) == 0)
 				throw new MotionChangeException("Tried to set initiator to empty set of zones");
+			
+			// If input is good...
 			this.initiators = initiators;
 		}
 
 		protected internal void setTargets(Zone target) {
+			// Check input
 			if (target == null)
 				throw new MotionChangeException("Tried to set target to null zone");
+			
+			// If input is good...
 			targets = ToSingletonArray(target);
 		}
 
 		protected internal void setTargets(Zone[] targets) {
+			// Check input
 			if (targets == null)
 				throw new MotionChangeException("Tried to set target to null zones");
 			if (getNumOccupants(targets) == 0)
 				throw new MotionChangeException("Tried to set target to empty set of zones");
+
+			// If input is good...
 			this.targets = targets;
 		}
 
