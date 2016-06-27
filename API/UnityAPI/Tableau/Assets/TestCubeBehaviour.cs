95b582f8260872e6b0491e09bf41bd1b8955b106
nanflasted <nanflasted@gmail.com>
2016-06-27 12:15:15 -0400

    Unity + Emulator Test
    Amazing moving cube, that is.

---
+++ API/UnityAPI/Tableau/Assets/TestCubeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCubeBehaviour : MonoBehaviour {
+    int i = 0;
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(cube.name);
+        Debug.Log(cube.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        i++;
+        float newX = i/100f;
+        cube.transform.position = new Vector3(newX,cube.transform.position.y, cube.transform.position.z);
+        Debug.Log(cube.transform.position);
+        Debug.Log(newX);
+	}
+}
