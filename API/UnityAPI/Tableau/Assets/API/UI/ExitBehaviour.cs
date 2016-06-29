e5d09cbfab53a7868a7e2d69ef97d939cbcce485
nanflasted <nanflasted@gmail.com>
2016-06-29 18:06:13 -0400

    Basic Unity Scene structures

---
+++ API/UnityAPI/Tableau/Assets/API/UI/ExitBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitBehaviour : MonoBehaviour {
+    public GameObject button;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
