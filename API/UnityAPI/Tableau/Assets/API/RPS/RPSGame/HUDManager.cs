528d86f6e63485749cc7fa97a832aabcc31f3c6f
Matt Conley <Matt Conley>
2016-08-02 23:18:21 -0400

    Added Gui Canvas to RPSGame, initial framework for HUD

---
+++ API/UnityAPI/Tableau/Assets/API/RPS/RPSGame/HUDManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HUDManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
