8a1c35f11e89e256491677eafefd63098f247c82
Matt Conley <Matt Conley>
2016-08-06 16:06:53 -0400

    Added GUI without music based on RPSGUI.  Not sure if it works, everything is the same except for the material, title text, and placement.  Next step is to find music and sync that.

---
+++ API/UnityAPI/Tableau/Assets/API/Othello/OthelloGUI/OthelloButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OthelloButtonManager : MonoBehaviour {
+
+	public void OnStartGame()
+    {
+        
+        SceneManager.LoadScene("RPSGame");
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnGazeEnter(GameObject o)
+    {
+        o.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
+    }
+
+    public void OnGazeExit(GameObject o)
+    {
+        o.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+    }
+
+    /*
+     * not working. There might be a way to do this without having to add a reference in the Editor
+     * but will have to figure it out
+    public void OnGazeEnter()
+    {
+        Debug.Log(GetComponentInParent<GameObject>().name);
+        Debug.Log(gameObject.name);
+    }
+    */
+}
