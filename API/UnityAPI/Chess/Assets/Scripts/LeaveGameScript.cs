fa50ca5a1dfeb1ebdfd780943235f5e44fcad0b3
jwf67 <jwf67@case.edu>
2016-09-08 00:25:06 -0400

    Chess Demo Models
    Added some scripts and mdoels for chess.

---
+++ API/UnityAPI/Chess/Assets/Scripts/LeaveGameScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LeaveGameScript : MonoBehaviour
+{ 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //exit the game
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ API/UnityAPI/Chess/Assets/Scripts/TitleCubeMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleCubeMovement : MonoBehaviour {
+
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rotating the cube in different directions
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.Rotate(Vector3.right, speed * Time.deltaTime);
+
+        //enter the game screen
+        if (Input.GetKey(KeyCode.Space)){
+            SceneManager.LoadScene(1);
+        }
+        //exit the game
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
